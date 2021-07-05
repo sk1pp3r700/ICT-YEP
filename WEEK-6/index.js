20ace9f839e0b0fbc5cb93a5384ddf9f71f142c4
sk1pp3r700 <eandre70@gmail.com>
2021-07-05 11:45:32 +0100

    final

--- WEEK-6/index.js
+++ WEEK-6/index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const port = 5000;
 const path = require('path')
+
 // app.get('/', (req, res) => {
 //   res.send('<h2>SKIPPERS SITE</h2>')
 // })
+
 app.get('/' ,(req, res) => {
     res.sendFile(path.join(__dirname, "public" ,  "index.html"));
 })
-  app.listen(port, () => {
-    console.log(`server broadcasting on http://127.0.0.1:${port}`)})
+
+const users = [{name: 'John', age: 34},
+{name: 'Mary', age: 33 }]
+
+app.get("/json" , (req, res) => {
+res.json(users)
+})
+
+app.get('*' ,(req, res) => {
+    res.send("404 Not Found")});
+
+app.listen(port, () => {
+console.log(`server broadcasting on http://127.0.0.1:${port}`)})
--- WEEK-6/test.js
+++ WEEK-6/test.js
@@ -1,15 +1,3 @@
-var express = require('express');  
-var app = express();  
-app.get('/', function (req, res) {  
-  res.send('Welcome to MY SITE!');  
-});  
-var server = app.listen(8000, function () {  
-  var host = server.address().address;  
-  var port = server.address().port;  
-  console.log('Example app listening at http://%s:%s', host, port);  
-});  
-
-
 // const express = require('express')
 // const app = express()
 // const port = 3000
